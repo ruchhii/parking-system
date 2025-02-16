ee1a956d285725cec455c05d0eda7a4f606cd7a7
Ruchi Singh <160203099+ruchhii@users.noreply.github.com>
2025-02-17 00:59:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "frontend")));
+
+// ✅ Default route to serve index.html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
 // ✅ Create MySQL Connection
 const db = mysql.createConnection({
     host: "interchange.proxy.rlwy.net",
